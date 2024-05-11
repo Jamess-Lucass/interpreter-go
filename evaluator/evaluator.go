e88521c861e7c0df2bed324d8eace304e149c08d
Jamess-Lucass <23193271+Jamess-Lucass@users.noreply.github.com>
2024-05-11 23:26:57 +0100

    added puts function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -110,6 +110,15 @@ var builtins = map[string]*object.Builtin{
 			return &object.Array{Elements: newElements}
 		},
 	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
+		},
+	},
 }
 
 func newError(format string, a ...interface{}) *object.Error {
